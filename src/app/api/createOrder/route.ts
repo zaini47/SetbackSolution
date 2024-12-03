24c1dd97078f8e5197afc9d56afdcd6cb7115d9a
Muhammad zain <mzain.virtuenetz@gmail.com>
2024-12-03 07:40:21 +0500

    test push

--- src/app/api/createOrder/route.ts
+++ src/app/api/createOrder/route.ts
@@ -5,7 +5,7 @@ if (!process.env.RAZORPAY_KEY_ID || !process.env.RAZORPAY_KEY_SECRET) {
   throw new Error("Razorpay environment variables are missing");
 }
 const razorpay = new Razorpay({
-  key_id: process.env.RAZORPAY_KEY_ID as string, // Use non-prefixed env variables
+  key_id: process.env.NEXT_PUBLIC_RAZORPAY_KEY_ID as string, // Use non-prefixed env variables
   key_secret: process.env.RAZORPAY_KEY_SECRET || '',
 });
 
